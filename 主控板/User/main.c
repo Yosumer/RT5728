e8e0020637b0766d0d5d579df5696987e111a0cf
wangqian <541803942@qq.com>
2019-01-07 22:15:39 +0800

    222

--- 主控板/User/main.c
+++ 主控板/User/main.c
@@ -1,8 +1,51 @@
 #include "Include.h"
 RT_Flag Flag;
+
 /*******************************************************************************
-*       Main Function
+*       
 *******************************************************************************/
+
+unsigned char nChairRunState;
+
+
+
+/*******************************************************************************
+*       
+*******************************************************************************/
+void Main_Settle(void);
+void Sleep_Process(void);
+void Main_WaitCommand(void);
+void Main_Work_Run(void);
+
+
+
+
+
+
+
+/*******************************************************************************
+*      Function
+*******************************************************************************/
+void Main_Settle(void)
+{
+
+}
+
+void Sleep_Process(void)
+{
+
+}
+
+void Main_WaitCommand(void)
+{
+
+}
+
+void Main_Work_Run(void)
+{
+
+}
+
 void main (void)
 {
     Sys_Init();
@@ -12,5 +55,22 @@ void main (void)
     {
         set_WDCLR;
         Comm();
+        switch (nChairRunState)
+        {
+            case CHAIR_STATE_SETTLE:
+                Main_Settle();
+                break;
+            case CHAIR_STATE_SLEEP:
+                Sleep_Process();
+                break;
+            case CHAIR_STATE_WAIT_COMMAND:
+                Main_WaitCommand();
+                break;
+            case CHAIR_STATE_RUN:
+                Main_Work_Run();
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ 主控板/User/main.h
@@ -0,0 +1,14 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+
+
+#define CHAIR_STATE_SETTLE          1  //回位状态
+#define CHAIR_STATE_SLEEP           2  //睡眠模式
+#define CHAIR_STATE_WAIT_COMMAND    3  //等待命令
+#define CHAIR_STATE_RUN             4  //运行状态
+
+
+
+#endif
+
